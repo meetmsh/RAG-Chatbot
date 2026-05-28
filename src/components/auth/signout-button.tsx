d9564b9c3dfa9fef6331578f3557be118b97d560
Meet Master <meetmaster16@gmail.com>
2026-05-27 23:08:23 -0400

    feat: integrate drizzle adapter for authentication and add signout button component

---
+++ src/components/auth/signout-button.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { authClient } from '@/lib/auth-client';
+import { useRouter } from 'next/navigation';
+import { Button } from '../ui/button';
+
+export const SignoutButton = () => {
+  const router = useRouter();
+
+  const signout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => router.push('/login'),
+      },
+    });
+  };
+
+  return (
+    <Button onClick={signout} variant="outline">
+      Sign out
+    </Button>
+  );
+};
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,15 @@
+import { db } from '@/db';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { betterAuth } from 'better-auth';
+import { headers } from 'next/headers';
 
 export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+  }),
+  pages: {
+    signIn: '/login',
+  },
   socialProviders: {
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
@@ -12,3 +21,8 @@ export const auth = betterAuth({
     },
   },
 });
+
+export const getSession = async () =>
+  auth.api.getSession({
+    headers: await headers(),
+  });
